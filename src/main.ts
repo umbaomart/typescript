7a15510f3699cb64dcf45133743b70b17cff998b
dev_Mart <martumbaocommude@gmail.com>
2024-01-11 20:14:09 +0800

    Refactor variable declarations and add code comments

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
+// Assigning types to variables
+// const a = 2;
+// let b: number = 4
+// let c = 5
+// let str: string = 'Hello'
+// const n = null
+// const u = undefined
 
-const a = 2;
-let b: number = 4
-let c = 5
-let str: string = 'Hello'
+// Array
+// const a: number[] = [1, 2, 3] // array of numbers
+// a.push(4)
+// or
+// let a: number[] = []
+// a = [1, 2, 3]
 
-const n = null
-n = undefined
+// Date
+// const today: Date = new Date()
 
-const u = undefined
-u = null
+// Any Type // Never use any bad code for TypeScript
+// let a // let a: any = 4
+// a = 3
+// a = "Hello"
+// const a = fetch('sdfds').then(res => res.json()).then(data => {
+//   data
+// })
