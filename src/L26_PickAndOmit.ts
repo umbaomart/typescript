6281ce68c6e2f41f54ba1a7c52c478dd2739f264
dev_Mart <martumbaocommude@gmail.com>
2024-01-16 10:23:49 +0800

    Pick and Omit Type

---
+++ src/L26_PickAndOmit.ts
@@ -0,0 +1,22 @@
+type Todo = {
+    id: string
+    name: string
+    status: string
+    completed: boolean
+}
+
+// type NewTodo = Pick<Todo, "name" | "status"> // Pick only name and status
+type NewTodo = Omit<Todo, "id">
+
+function saveTodo(todo: Todo): Todo {
+    // todo.id = crypto.randomUUID()
+    // return todo // or
+    return { ...todo, id: crypto.randomUUID() }
+}
+
+function renderTodo(todo: Todo) {
+    const div = document.createElement("div");
+    div.id = todo.id
+    // DO the rest
+
+}
