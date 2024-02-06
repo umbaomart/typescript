a656f76dd0fe730f7cd24680416b4f51f1182e46
dev_Mart <martumbaocommude@gmail.com>
2024-02-06 19:43:18 +0800

    Add data resource class

--- pizza-app/src/ts/index.ts
+++ pizza-app/src/ts/index.ts
@@ -1,1 +1,8 @@
+import { Pizza } from "./models/Pizza";
 
+document.addEventListener('DOMContentLoaded', async () => {
+    // Load the pizza data
+    const pizzas = await Pizza.loadAll();
+
+    console.log(pizzas);
+})
